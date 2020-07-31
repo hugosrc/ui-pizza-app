6ddee80f5862fb526026b7430da9c4f190cf1d78
hugosrc <hugo.uibai54@gmail.com>
2020-07-31 11:55:03 -0300

    Configures light theme colors

--- App.tsx
+++ App.tsx
@@ -1,12 +1,15 @@
-import { StatusBar } from 'expo-status-bar';
 import React from 'react';
+import { StatusBar } from 'expo-status-bar';
+import { ThemeProvider } from 'styled-components';
+
+import { light } from './src/styles/theme/light';
 import Routes from './src/routes'
 
 export default function App() {
   return (
-    <>
-      <Routes/>
+    <ThemeProvider theme={light}>
       <StatusBar style="auto" />
-    </>
+      <Routes/>
+    </ThemeProvider>
   );
 }
---
+++ src/styles/theme/light.ts
@@ -0,0 +1,11 @@
+import { DefaultTheme } from "styled-components";
+
+export const light: DefaultTheme = {
+  colors: {
+    primary: '#FC4C4C',
+    secondary: '#231E36',
+    background: '#EFEFEF',
+    gray: '#625F6F',
+    white: '#FFFFFF'
+  }
+}
